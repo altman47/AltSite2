fb359ec851d1ee9c24f060baae00c3df09b0b4e4
Joe Altmann <jaltmann89@gmail.com>
2021-12-10 14:48:40 -0500

    Some tutorial work

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number = 4;
+  btnText: string = 'Add an item';
+  goalText: string = 'My first life goal';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
